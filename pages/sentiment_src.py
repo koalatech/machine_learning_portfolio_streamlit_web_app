4e8136206c38302d8a0f5ca74a4b98fe28a6ff95
Richard M. Pabelona Jr <koala.linux@gmail.com>
2024-06-06 15:45:59 +0800

    Update sentiment_src.py
    updated title

--- pages/sentiment_src.py
+++ pages/sentiment_src.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.header('Simple Sentiment Analyzer')
+st.header('Simple Sentiment Analyzer App')
 st.subheader('This python code is implemented for Streamlit')
 st.code('''
         import streamlit as st
