f6aa3f0fc645ed3bbeaf21633903bc795c65c07f
Richard M. Pabelona Jr <koala.linux@gmail.com>
2024-06-15 11:03:11 +0800

    Update basic_sentiment_analyzer.py
    commented @st.cache_data

--- pages/basic_sentiment_analyzer.py
+++ pages/basic_sentiment_analyzer.py
@@ -13,14 +13,14 @@ filename = 'pages/sentimentAnalyzerTest_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 
 # Define features (words) and their corresponding labels (positive/negative)
-@st.cache_data 
+# @st.cache_data 
 def word_features(words):
     return dict([(word, True) for word in words])
 
 message_tone = loaded_model.classify(word_features(message.split()))
 
 # make a function for your button click
-@st.cache_data 
+# @st.cache_data 
 def sayFeeling():
     # Classify the sentiment
     if message_tone == 'positive':
